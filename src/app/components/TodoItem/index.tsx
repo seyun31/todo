74a065ef7c5815458a0061cf391c54c87f8065d5
seyun31 <2ne1jenna@naver.com>
2024-01-05 02:56:22 +0900

    Todo delete button, Todo reducers for slice pattern

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import Checkbox from '../CheckBox';
 
 import Block from '../Block';
+import CircleButton from '../Button/CircleButton';
+import TodoInput from '../TodoInput';
 const Box = styled.div`
   display: flex;
   align-items: center;
@@ -11,6 +13,17 @@ const Box = styled.div`
   width: 100%;
   font-size: 1.2em;
   border-bottom: 1px solid #eee;
+
+  & > .delete-button {
+    display: none;
+  }
+
+  &:hover {
+    padding: 10px 15px 10px 25px;
+    & > .delete-button {
+      display: flex;
+    }
+  }
 `;
 
 const TodoContent = styled.span<{ checked: boolean }>`
@@ -27,9 +40,31 @@ const TodoContent = styled.span<{ checked: boolean }>`
 export default function TodoItem({ todo }: { todo: ITodoItem }) {
   return (
     <Box>
-      <Checkbox checked={todo.completed} />
-      <Block marginLeft="10px" />
-      <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+      <div style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <Checkbox checked={todo.completed} />
+        <Block marginLeft="10px" />
+        {todo.editing ? (
+          <TodoInput />
+        ) : (
+          <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+        )}
+        <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+      </div>
+
+      <CircleButton
+        className="delete-button"
+        onClick={() => {}}
+        Icon={() => (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+          </svg>
+        )}
+      />
     </Box>
   );
 }
---
+++ src/store/todo/index.ts
@@ -0,0 +1,70 @@
+import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit';
+import { TodoState } from './types';
+import { useInjectReducer } from 'redux-injectors';
+
+export const initalState: TodoState = {
+  todolist: [],
+};
+
+const slice = createSlice({
+  name: 'todo',
+  initialState: initalState,
+  reducers: {
+    addTodo: {
+      reducer: (state, acction: PayloadAction<ITodoItem>) => {
+        state.todolist.push(acction.payload);
+      },
+      prepare: (content: string) => {
+        const id = nanoid();
+        return {
+          payload: {
+            id: id,
+            content: content,
+            completed: false,
+            editing: false,
+          },
+        };
+      },
+      checkTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.completed = !todo.completed;
+        }
+      },
+      editModeTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+
+        for (const todo of state.todolist) {
+          if (todo.id === id) continue;
+          if (todo.editing === true) todo.editing = false;
+        }
+
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.editing = !todo.editing;
+        }
+      },
+      editTodo(state, action: PayloadAction<{ id: string; content: string }>) {
+        const id = action.payload.id;
+        const content = action.payload.content;
+
+        const todo = state.todolist.find(todo => todo.id === id);
+        if (todo) {
+          todo.content = content;
+        }
+      },
+      deleteTodo(state, action: PayloadAction<{ id: string }>) {
+        const id = action.payload.id;
+        const filteredTodos = state.todolist.filter(todo => todo.id !== id);
+        state.todolist = filteredTodos;
+      },
+    },
+  },
+});
+
+export const { actions: TodoActions } = slice;
+export const useTodoSlice = () => {
+  useInjectReducer({ key: slice.name, reducer: slice.reducer });
+  return { TodoActions: slice.actions };
+};
